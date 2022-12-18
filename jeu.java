e61af8e5c10d186323dc93b3bd5f939b678b7a27
Hokle <115533230+Hokle@users.noreply.github.com>
2022-12-18 20:49:44 +0100

    Jeu.java

--- jeu.java
+++ jeu.java
@@ -1,13 +1,16 @@
+import extensions.File;
+import extensions.CSVFile;
+
 class jeu extends Program{
+    int affichLgth = 0;
+    CSVFile bossCSV = loadCSV("./boss/boss.csv");
     
     void algorithm(){
         /// Système de sauvegarde à X joueurs
         /// Système d'inventaire
-        int int1 = readInt();
-        char operateur = readChar();
-        int int2 = readInt();
-        Operation test = creerOperation(int1, operateur, int2);
-        printOperation(test);
+        String bossSelected = readString();
+        Boss boss = creerBoss(bossSelected);
+        printBoss(boss, initPrintBoss(bossSelected));
     }
     
     Operation creerOperation(int int1, char operateur, int int2){
@@ -45,19 +48,80 @@ class jeu extends Program{
         println();
     }
 
-    Boss creerBoss(int pv, int force, String nom, String[] design) {
+    Boss creerBoss(String nom) {
         Boss newBoss = new Boss();
-        newBoss.pv = pv;
-        newBoss.force = force;
-        newBoss.nom = nom;
-        newBoss.design = design;
+        int i = 0;
+        while (i < rowCount(bossCSV) && newBoss.nom != nom) {
+            if (nom != getCell(bossCSV, i, 2)) {
+                newBoss.pv = stringToInt(getCell(bossCSV, i, 0));
+                newBoss.force = stringToInt(getCell(bossCSV, i, 1));
+                newBoss.nom = getCell(bossCSV, i, 2);
+            }
+            i++;
+        }
         return newBoss;
     }
 
-    void printBoss(Boss boss) {
-        for (int i = 0 ; i < length(boss.design) ; i++) {
-            println(boss.design[i]);
+    String[] initPrintBoss (String boss) {
+        File asciiArt = newFile("boss/" + boss + ".txt"); 
+        int i = 0;
+        while(ready(asciiArt)) {
+            String currentLine = readLine(asciiArt);
+            if (length(currentLine) > affichLgth) affichLgth = length(currentLine);
+            i++;
         }
+        String[] tabBoss = new String[i];
+        File asciiArt2 = newFile("boss/" + boss + ".txt"); 
+        int j = 0;
+        while(ready(asciiArt2)) {
+            String currentLine2 = readLine(asciiArt2);
+            tabBoss[j++] = currentLine2;
+        }
+        return tabBoss;
+    }
+
+    void printTab(String[] boss) {
+        for (int i = 0 ; i < length(boss) ; i++) {
+            print("║ ");
+            print(boss[i]);
+            if (affichLgth > length(boss[i])) {
+                for (int j = 0 ; j < affichLgth - length(boss[i]) ; j++) print(' ');
+            }
+            println(" ║");
+        }
+    }
+
+    void printCaracteristique(String stat, String nStat) {
+        String printedStat = "║ " + stat + nStat;
+        print(printedStat);
+        for(int i = 0 ; i < affichLgth - length(printedStat) ; i++) {
+            print(' ');
+        }
+        print("   ║");
         println();
+    }
+
+
+    void printBoss(Boss boss, String[] design) {
+        clearScreen();
+        cursor(1,1);
+        print("╔═");
+        String ligneAffich = "";
+        for(int i = 0 ; i < affichLgth ; i++) {
+            ligneAffich += '═';
+        }
+        print(ligneAffich);
+        println("═╗");
+        printTab(design);
+        printCaracteristique("", boss.nom);
+        String bossPv = "";
+        bossPv += boss.pv;
+        printCaracteristique("Vies = ", bossPv);
+        String bossForce = "";
+        bossForce += boss.force;    
+        printCaracteristique("Force = ", bossForce);
+        print("╚═");
+        print(ligneAffich);
+        println("═╝");
     }
 }
